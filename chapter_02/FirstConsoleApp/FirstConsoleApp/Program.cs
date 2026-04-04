fa1496c114e015194cfebc60e47a66d47dd360b9
Maria Fernanda Leonel Bertelli <mfbertelli@hotmail.com>
2026-04-04 13:54:05 -0300

    chapter 1 complete and half of chapter 2

---
+++ chapter_02/FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperatorExamples();
+        }
+
+        private static void OperatorExamples()
+        {
+            int width = 3;
+
+            width++;
+
+            int height = 2 + 4;
+            int area = width * height;
+            Console.WriteLine(area);
+
+            while (area < 50)
+            {
+                height++;
+                area = width * height;
+            }
+
+            do
+            {
+                width--;
+                area = width * height;
+            } while (area > 25);
+
+            string result = "The area";
+            result = result + " is " + area;
+            Console.WriteLine(result);
+
+            bool truthValue = true;
+            Console.WriteLine(truthValue);
+
+        }
+    }
+}
