9a372a42de7a859da7cb1efdb6f450b2c21819fe
Maria Fernanda Leonel Bertelli <mfbertelli@hotmail.com>
2026-04-12 20:38:38 -0300

    .gitignore is right now (unnecessary files removed)

---
+++ chapter_03/PickRandomCards/PickRandomCards/CardPicker.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PickRandomCards
+{
+    public class CardPicker
+    {
+        static Random random = new Random();
+        public static string[] PickSomeCards (int numberOfCards)
+        {
+            string[] pickedCards = new string[numberOfCards];
+            for (int i = 0; i < numberOfCards; i++) {
+                pickedCards[i] = RandomValue() + " of " + RandomSuit();
+            }
+            return pickedCards;
+        }
+
+        private static string RandomSuit()
+        {
+            int value = random.Next(1, 5);
+            if (value == 1) return "Spades";
+            else if (value == 2) return "Hearts";
+            else if (value == 3) return "Clubs";
+            else return "Diamonds";
+        }
+
+        private static string RandomValue()
+        {
+            int value = random.Next(1, 14);
+            if (value == 1) return "Ace";
+            else if (value == 11) return "Jack";
+            else if (value == 12) return "Queen";
+            else if (value == 13) return "King";
+            else return value.ToString();
+        }
+    }
+}
