8074002ed5a900d6aa3ef566935ea3bcc1758939
Maria Fernanda Leonel Bertelli <mfbertelli@hotmail.com>
2026-04-22 22:02:19 -0300

    ability score almost finished

---
+++ chapter_04/AbilityScore/AbilityScore/AbilityScoreCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace AbilityScore
+{
+    public class AbilityScoreCalculator
+    {
+        public int RollResult = 14;
+        public double DividedBy = 1.75;
+        public int AddAmount = 2;
+        public int Minimum = 3;
+        public int Score;
+
+        public void CalculateAbilityScore()
+        {
+            double divided = RollResult / DividedBy;
+            int added = AddAmount += (int)divided;
+            if (added < Minimum)
+                Score = Minimum;
+            else
+                Score = added;
+        }
+    }
+}
---
+++ chapter_04/AbilityScore/AbilityScore/Program.cs
@@ -0,0 +1,44 @@
+﻿using AbilityScore;
+
+
+AbilityScoreCalculator calculator = new AbilityScoreCalculator();
+while (true)
+{
+    calculator.RollResult = ReadInt(calculator.RollResult, "Starting 4d6 roll");
+    calculator.DividedBy = ReadDouble(calculator.DividedBy, "Divided by");
+    calculator.AddAmount = ReadInt(calculator.AddAmount, "Add amount");
+    calculator.Minimum = ReadInt(calculator.Minimum, "Minimum");
+    calculator.CalculateAbilityScore();
+    Console.WriteLine($"Calculated ability score: {calculator.Score}");
+    Console.WriteLine("Press Q to quit, any other key to continue.");
+    char keyChar = Console.ReadKey(true).KeyChar;
+    if ((keyChar == 'Q') || (keyChar == 'q')) return;
+}
+
+static int ReadInt(int lastUsedValue, string prompt)
+{
+    Console.WriteLine($"{prompt} [{lastUsedValue}]:");
+    string input = Console.ReadLine();
+    if (int.TryParse(input, out int result))
+    {
+        Console.WriteLine($"using value {result}");
+        return result;
+    }
+
+    Console.WriteLine($"using default value {lastUsedValue}");
+    return lastUsedValue;
+}
+
+static double ReadDouble(double lastUsedValue, string prompt)
+{
+    Console.WriteLine($"{prompt} [{lastUsedValue}]:");
+    string input = Console.ReadLine();
+    if (double.TryParse(input, out double result))
+    {
+        Console.WriteLine($"using value {result}");
+        return result;
+    }
+
+    Console.WriteLine($"using default value {lastUsedValue}");
+    return lastUsedValue;
+}
