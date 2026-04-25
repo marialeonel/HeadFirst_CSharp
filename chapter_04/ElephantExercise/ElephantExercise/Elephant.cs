3eeed582061527912fbf4efaa34eb9c9c8183598
Maria Fernanda Leonel Bertelli <mfbertelli@hotmail.com>
2026-04-25 16:04:12 -0300

    Elephant exercise created

---
+++ chapter_04/ElephantExercise/ElephantExercise/Elephant.cs
@@ -0,0 +1,14 @@
+﻿namespace ElephantExercise
+{
+    public class Elephant
+    {
+        public string Name;
+        public int EarSize;
+
+        public string WhoAmI()
+        {
+            return $"I am an elephant 🐘! My name is {Name} and my ears are {EarSize} inches tall.\n";
+        }
+
+    }
+}
---
+++ chapter_04/ElephantExercise/ElephantExercise/Program.cs
@@ -0,0 +1,40 @@
+﻿using ElephantExercise;
+using System.Text;
+
+Console.OutputEncoding = Encoding.UTF8;
+
+Elephant lloyd = new Elephant() { Name = "Lloyd", EarSize = 40 };
+Elephant lucynda = new Elephant() { Name = "Lucynda", EarSize = 33 };
+
+Console.WriteLine("Press 1 for Lloyd, 2 for Lucynda, 3 to swap: ");
+while(true)
+{
+    string userChoice = Console.ReadLine();
+
+    if (int.TryParse(userChoice, out int result))
+    {
+        switch (result)
+        {
+            case 1:
+                Console.WriteLine(lloyd.WhoAmI());
+                break;
+            case 2:
+                Console.WriteLine(lucynda.WhoAmI());
+                break;
+            case 3:
+                Elephant intermediary = lloyd;
+                lloyd = lucynda;
+                lucynda = intermediary;
+                Console.WriteLine("References have been swapped!\n");
+                break;
+            default:
+                Console.WriteLine("Invalid choice. Please enter 1, 2, or 3.\n");
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Invalid input. Please enter a number.\n");
+    }
+}
+
