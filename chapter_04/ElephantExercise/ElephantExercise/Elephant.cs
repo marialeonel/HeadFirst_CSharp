bcb15319357d26a78757be7baa626b2e89a17229
Maria Fernanda Leonel Bertelli <mfbertelli@hotmail.com>
2026-04-28 22:25:23 -0300

    ElephantExercise and RandomTestDriveFinalized

--- chapter_04/ElephantExercise/ElephantExercise/Elephant.cs
+++ chapter_04/ElephantExercise/ElephantExercise/Elephant.cs
@@ -10,5 +10,16 @@
             return $"I am an elephant 🐘! My name is {Name} and my ears are {EarSize} inches tall.\n";
         }
 
+        public void HearMessage(string message,Elephant whoSaidThat)
+        {
+            Console.WriteLine(Name  + " heard a message");
+            Console.WriteLine($"{whoSaidThat.Name} said this: {message}");
+        }
+
+         public void SpeakTo(string message, Elephant whoToSpeakTo)
+        {
+            whoToSpeakTo.HearMessage(message, this);
+        }
+ 
     }
 }
--- chapter_04/ElephantExercise/ElephantExercise/Program.cs
+++ chapter_04/ElephantExercise/ElephantExercise/Program.cs
@@ -27,6 +27,21 @@ while(true)
                 lucynda = intermediary;
                 Console.WriteLine("References have been swapped!\n");
                 break;
+            //case 4:
+            //    lloyd = lucynda;
+            //    lloyd.EarSize = 4321;
+            //    lloyd.WhoAmI();
+            //    break;
+            /* apenas para teste do que acontece quando lloyd 
+             * recebe a referência de lucynda e depois tem seu EarSize modificado.
+             * O resultado é que tanto lloyd quanto lucynda terão o EarSize modificado, 
+             * pois ambos apontam para o mesmo objeto na memória. 
+             * Portanto, ao modificar o EarSize através de lloyd, 
+             * você também estará modificando o EarSize do objeto referenciado por lucynda, 
+             * já que ambos estão apontando para o mesmo objeto.*/
+            case 5:
+                lucynda.SpeakTo("Hi Lloyd, how are you?", lloyd);
+                break;
             default:
                 Console.WriteLine("Invalid choice. Please enter 1, 2, or 3.\n");
                 break;
