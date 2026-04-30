49380eda6ba26e4b04e3942c3b01dd52523c9e44
Maria Fernanda Leonel Bertelli <mfbertelli@hotmail.com>
2026-04-29 22:32:19 -0300

    sloppy joe initialized with a different way  when setting the textblocks

---
+++ chapter_04/SloppyJoe/SloppyJoe/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SloppyJoe
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            MakeTheMenu();
+        }
+
+        private void MakeTheMenu()
+        {
+            MenuItem[] menuItems = new MenuItem[5];
+            String guacamolePrice;
+
+            for(int i = 0; i < 5; i++)
+            {
+                menuItems[i] = new MenuItem();
+                if(i >= 3)
+                {
+                    menuItems[i].Breads = [
+                        "plain bagel", "onion bagel", "pumpernickel bagel", "everything bagel"
+                    ];
+                }
+
+                menuItems[i].Generate();
+            }
+
+            var items = GridMenuItem.Children
+                .OfType<TextBlock>()
+                .Where(tb => tb.Name.StartsWith("item") && !tb.Name.EndsWith("6"))
+                .OrderBy(tb => tb.Name)
+                .ToList();
+
+            var prices = GridMenuItem.Children
+                .OfType<TextBlock>()
+                .Where(tb => tb.Name.StartsWith("price") && !tb.Name.EndsWith("6"))
+                .OrderBy(tb => tb.Name)
+                .ToList();
+
+            for (int i = 0; i < 5; i++)
+            {
+                items[i].Text = menuItems[i].Description;
+                prices[i].Text = menuItems[i].Price.ToString();
+            }
+
+            MenuItem specialMenuItem = new MenuItem()
+            {
+                Proteins = ["Organic ham", "Mushroom patty", "Mortadella"],
+                Breads = ["a gluten free roll", "a wrap", "pita"],
+                Condiments = ["dijon mustard", "miso dressing", "au jus"]
+            };
+
+            specialMenuItem.Generate();
+
+            item6.Text = specialMenuItem.Description;
+            price6.Text = specialMenuItem.Price;
+
+            MenuItem guacamoleMenuItem = new MenuItem();
+            guacamoleMenuItem.Generate();
+            guacamolePrice = guacamoleMenuItem.Price;
+
+            guacamole.Text = $"Add guacamole for {guacamolePrice}";
+        }
+    }
+}
---
+++ chapter_04/SloppyJoe/SloppyJoe/MenuItem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Permissions;
+using System.Text;
+
+namespace SloppyJoe
+{
+    class MenuItem
+    {
+        public Random Randomizer = new Random();
+        public string[] Proteins = { "Roast Beef", "Salami", "Turkey", "Ham", "Pastrami", "Tofu" };
+        public string[] Condiments = { "Yellow Mustard", "Brown Mustard", "Honey Mustard", "Mayo", "Relish", "French dressing" };
+        public string[] Breads = {"White", "Wheat", "Rye", "Sourdough", "Seven Grain"};
+
+        public string Description = "";
+        public string Price;
+
+        public void Generate()
+        {
+            string randomProtein = Proteins[Randomizer.Next(Proteins.Length)];
+            string randomCondiment = Condiments[Randomizer.Next(Condiments.Length)];
+            string randomBread = Breads[Randomizer.Next(Breads.Length)];
+            Description = $"{randomProtein} with {randomCondiment} on {randomBread}";
+
+            decimal bucks = Randomizer.Next(2, 5);
+            decimal cents = Randomizer.Next(1, 98);
+            decimal price = bucks + (cents * .01M);
+            Price = price.ToString("c");
+        }
+    }
+}
