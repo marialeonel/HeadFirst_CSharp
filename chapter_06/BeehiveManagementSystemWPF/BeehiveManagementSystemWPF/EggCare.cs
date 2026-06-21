ef1ff444bd131c7122b510bbf9435e9def4e8f1d
Maria Fernanda Leonel Bertelli <mfbertelli@hotmail.com>
2026-06-21 10:08:17 -0300

    BeehiveManagementSystem completely finished (with abstract declarations)

---
+++ chapter_06/BeehiveManagementSystemWPF/BeehiveManagementSystemWPF/EggCare.cs
@@ -0,0 +1,19 @@
+﻿namespace BeehiveManagementSystemWPF
+{
+    public class EggCare : Bee
+    {
+        public EggCare(Queen _queen) : base("Egg Care")
+        {
+            queen = _queen;
+        }
+
+        private Queen queen;
+
+        const float CARE_PROGRESS_PER_SHIFT = 0.15f;
+        public override float CostPerShift { get { return 1.35f; } }
+        protected override void DoJob()
+        {
+            queen.CareForEggs(CARE_PROGRESS_PER_SHIFT);
+        }
+    }
+}
