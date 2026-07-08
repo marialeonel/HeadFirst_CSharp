e714d5a0f479684977236a411349b084f3fa8070
Maria Fernanda Leonel Bertelli <mfbertelli@hotmail.com>
2026-07-07 22:04:02 -0300

    ShoesConsole finished

---
+++ chapter_08/ShoesConsole/ShoesConsole/Style.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShoesConsole
+{
+    enum Style
+    {
+        Sneaker,
+        Loafer,
+        Sandal,
+        Flipflop,
+        Wingtip,
+        Clog,
+    }
+}
