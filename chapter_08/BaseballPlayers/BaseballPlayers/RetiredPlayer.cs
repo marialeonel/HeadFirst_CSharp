18a885eb52bb9c1f23d0e2fdc8d972a0a4d7126e
Maria Fernanda Leonel Bertelli <mfbertelli@hotmail.com>
2026-07-20 21:51:38 -0300

    BsaballPlayers (dictionary finalized)

---
+++ chapter_08/BaseballPlayers/BaseballPlayers/RetiredPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BaseballPlayers
+{
+    public class RetiredPlayer
+    {
+        public string Name { get; private set; }
+        public int YearRetired { get; private set; }
+
+        public RetiredPlayer(string name, int yearRetired)
+        {
+            Name = name;
+            YearRetired = yearRetired;
+        }
+
+
+
+    }
+}
