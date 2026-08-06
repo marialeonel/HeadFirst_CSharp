a91dbafe24fc64a63fc4c8de865f92bd5b9955fc
Maria Fernanda Leonel Bertelli <mfbertelli@hotmail.com>
2026-08-05 21:57:40 -0300

    comic console finalized

---
+++ chapter_09/ComicConsole/ComicConsole/Program.cs
@@ -0,0 +1,11 @@
+﻿using ComicConsole;
+
+IEnumerable<Comic> mostExpensive = from comic in Comic.Catalog
+                                   where Comic.Prices[comic.Issue] > 500
+                                   orderby -Comic.Prices[comic.Issue]
+                                   select comic;
+
+foreach (Comic comic  in mostExpensive)
+{
+    Console.WriteLine($"{comic} is worth {Comic.Prices[comic.Issue]:C}");
+}
