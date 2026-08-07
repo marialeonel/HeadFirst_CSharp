f7dadcc610cf316756d112415c94c20c21034c3c
Maria Fernanda Leonel Bertelli <mfbertelli@hotmail.com>
2026-08-06 21:43:20 -0300

    deferredEvaluation finalized

---
+++ chapter_09/DeferredEvaluation/DeferredEvaluation/Program.cs
@@ -0,0 +1,13 @@
+﻿using DeferredEvaluation;
+
+var listOfObjects = new List<PrintWhenGetting>();
+for (int i = 0; i < 5; i++)
+    listOfObjects.Add(new PrintWhenGetting { InstanceNumber = i });
+
+Console.WriteLine("Set up the query");
+var result = from o in listOfObjects select o.InstanceNumber;
+Console.WriteLine("Run the foreach");
+foreach(var number in result)
+    Console.WriteLine($"Writing number: {number}");
+
+
