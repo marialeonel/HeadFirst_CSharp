46d7892257f3d706690f60295fae3fd542b848eb
Maria Fernanda Leonel Bertelli <mfbertelli@hotmail.com>
2026-09-12 17:29:28 -0300

    LambdaTestDrive finalized

---
+++ chapter_09/LambdaTestDrive/LambdaTestDrive/LambdaTestDrive/Program.cs
@@ -0,0 +1,8 @@
+﻿Random random = new Random();
+double GetRandomDouble(int max) => max * random.NextDouble();
+
+void PrintValue(double d) => Console.WriteLine(d);
+
+var value = GetRandomDouble(100);
+PrintValue(value);
+
